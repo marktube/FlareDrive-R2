5243ddd62e5ac8e222dcedc95f7ee3dddc6d92f3
刘彦超 <liuyanchao99@gmail.com>
2025-08-29 16:35:31 +0800

    Update MediaPreview.vue
    fix click link

--- assets/MediaPreview.vue
+++ assets/MediaPreview.vue
@@ -183,7 +183,7 @@
               </svg>
             </button>
 
-            <button class="control-btn" @click="resetImage" title="全景模式">
+            <button class="control-btn" @click="panoImage" title="全景模式">
               <svg viewBox="0 0 24 24" width="20" height="20" fill="currentColor">
                 <path d="M5 5 L15 5 L15 15 L5 15 Z" />
               </svg>
